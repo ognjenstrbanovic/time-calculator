b3f8bed73cf284b7b9fa94a8f75953e9e24be57d
ognjenstrbanovic <ogistrbanovic@gmail.com>
2020-08-21 19:59:43 -0400

    progressing.

--- scripts/time_calculator.py
+++ scripts/time_calculator.py
@@ -1,15 +1,26 @@
 # dependencies
 import datetime
+from datetime import timedelta
+
 def add_time(start, duration, weekday = False):
 
-    if "AM" in start:
-        AM_start_cleaned = start.strip(" AM").split(":")
-    elif "PM" in start:
-        PM_start_cleaned = start.strip(" PM").split(":")
+    # reformatting 'start'; w/ help from GeeksforGeeks...
+    if start.strip()[-2:] == "AM" and start.strip()[:2] == "12":
+        new_start = "00" + start[2:-2]
+    elif start.strip()[-2:] == "AM":
+        new_start = start[:-2]
+    elif start.strip()[-2:] == "PM" and start.strip()[:2] == "12":
+        new_start = start[:-2]
+    else:
+        new_start = str(int(start.strip()[:2]) + 12) + start.strip()[2:5]
 
+    # found from kite.com...
+    start_time = datetime.time(hour=int(new_start[:2]), minute=int(new_start[-2:]))
+    if len(duration) == 4:
+        added_time = datetime.timedelta(hours=int(duration[:1]), minutes=int(duration[-2:]))
+    else:
+        added_time = datetime.timedelta(hours=int(duration[:2]), minutes=int(duration[-2:]))
 
-
-    return PM_start_cleaned
     # return new_time
 
 add_time("11:06 PM", "2:02")
