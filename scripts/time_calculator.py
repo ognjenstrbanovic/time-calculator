2738bfe954c2e6bab88a6e612fc96e8f2e394f6c
ognjenstrbanovic <ogistrbanovic@gmail.com>
2020-08-20 21:10:37 -0400

    added dependencies

--- scripts/time_calculator.py
+++ scripts/time_calculator.py
@@ -1,7 +1,15 @@
-def add_time(start, duration):
+# dependencies
+import datetime
+def add_time(start, duration, weekday = False):
+
+    if "AM" in start:
+        AM_start_cleaned = start.strip(" AM").split(":")
+    elif "PM" in start:
+        PM_start_cleaned = start.strip(" PM").split(":")
 
 
 
+    return PM_start_cleaned
+    # return new_time
 
-
-    return new_time
+add_time("11:06 PM", "2:02")
